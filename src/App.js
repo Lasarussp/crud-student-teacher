4ab3393dabdc075e09bf86aa5a5049b28f2257b3
Lasarussp <001lasarus@gmail.com>
2023-01-04 21:02:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Table from "./Components/Table/Table";
+import "./App.css";
+import Form from "./Components/Form/Form";
+import { Routes, Route } from "react-router-dom";
+import EditUser from "./Components/Edit/EditUser";
+import Login from "./Components/Login/Login";
+import TeacherTable from "./Components/TeacherTable";
+import CreateTeacher from './Components/CreateTeacher';
+import EditTeacher from "./Components/EditTeacher";
+import Home from "./Components/Home";
 
 function App() {
+  const [formList, setFormList] = useState([
+    {
+      id: 1,
+      name: "Lasarus",
+      email: "lasarus@gmail.com",
+      contact: 8778075566,
+    },
+    {
+      id: 2,
+      name: "Bob",
+      email: "bob@gmail.com",
+      contact: 9988998800,
+      },
+  ]);
+
+const [teacher,setTeacher] = useState([
+  {
+    id:1,
+    name:"Prabha",
+    email:"prabha@gmail.com",
+    subject:"React",
+    contact:8778075566,
+  },
+  {
+    id:2,
+    name:"Charlie",
+    email:"charlie@gmail.com",
+    subject:"Nodejs",
+    contact:9988776655,
+  }
+])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+      <Route
+          path="/"
+          element={<Login/>}
+        />
+        <Route
+          path="/home"
+          element={<Table formList={formList} setFormList={setFormList} />}
+        />
+        <Route
+          path="/main"
+          element={<Home/>}
+        />
+        <Route
+          path="/form"
+          element={<Form formList={formList} setFormList={setFormList} />}
+        />
+        <Route
+          path="/edit/:id"
+          element={<EditUser formList={formList} />}
+        />
+        <Route
+          path="/editmentor/:id"
+          element={<EditTeacher teacher={teacher} />}
+        />
+        <Route path="/form1" element={<CreateTeacher teacher={teacher} setTeacher={setTeacher} />} />
+        <Route path="/home1" element={<TeacherTable teacher={teacher} setTeacher={setTeacher} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="error">
+      <img
+        src="https://d1ivubrj2a21dq.cloudfront.net/wp-contact/uploads/2021//ezgif.com-crop.gif"
+        alt="404-Not-Found"
+      />
     </div>
   );
 }
